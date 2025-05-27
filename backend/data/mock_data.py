1a3546b3450a55104e64c8261ddfef93cf11edaa
Laís Domiciano <161219781+laistdomiciano@users.noreply.github.com>
2025-05-27 09:16:36 +0100

    Adding CampaignDataGenerator Class

--- backend/data/mock_data.py
+++ backend/data/mock_data.py
@@ -1,1 +1,11 @@
 
+
+
+class CampaignDataGenerator:
+    def __init__(self):
+        self.campaign_types = [
+            "Influencer Partnership", "Email Newsletter", "Social Media", 
+            "Podcast Sponsorship", "Content Marketing", "Affiliate Program"
+        ]
+        
+        self.markets = ["DACH", "US", "UK", "Global"]
